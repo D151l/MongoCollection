139f4319f5f8625814ae9fe97b3cf489ece39254
D151l <d151l@d151l.de>
2022-04-17 02:03:37 +0200

    Initial commit

---
+++ src/main/kotlin/de/d151l/mongodb/SpigotPlugin.kt
@@ -0,0 +1,19 @@
+package de.d151l.mongodb
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * @created 17/04/2022 - 01:34
+ * @project MongoCollection
+ * @author  D151l
+ */
+class SpigotPlugin: JavaPlugin() {
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
